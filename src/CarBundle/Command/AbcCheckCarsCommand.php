d4823a6e706ccbf9c3e1b83436df4e650c84acec
Alex van Niekerk <alex.vanniekerk@medbrief.co.uk>
2019-06-26 12:02:20 +0200

    Added / created service command in console

---
+++ src/CarBundle/Command/AbcCheckCarsCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace CarBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class AbcCheckCarsCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('abc:check-cars')
+            ->setDescription('...')
+            ->addArgument('argument', InputArgument::OPTIONAL, 'Argument description')
+            ->addOption('option', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $manager = $this->getContainer()->get('doctrine.orm.entity_manager');
+        $carRepository = $manager->getRepository('CarBundle:Car');
+        $cars = $carRepository->findAll();
+
+        foreach($cars as $car){
+            $output->writeln($car->getId());
+        }
+    }
+
+}
